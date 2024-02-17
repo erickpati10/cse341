c93f05f26ca877e5a3a8a0b5f6a93e87dfc66745
erickpati10 <pat21018@byui.edu>
2024-02-17 16:38:20 -0700

    conctacts code err fixed

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,7 +4,7 @@ const ObjectId = require("mongodb").ObjectId;
 const getAll = async (req, res) => {
   try {
     const result = await mongodb.getDb().db().collection("contacts").find();
-    result.toArray.then((lists) => {
+    result.toArray().then((lists) => {
       res.setHeader("Content-Type", "application/json");
       res.status(200).json(lists);
     });
