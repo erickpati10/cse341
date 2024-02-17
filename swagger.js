13f6b6759b9437743339f850a1643f60e22c408b
erickpati10 <pat21018@byui.edu>
2024-02-17 15:59:00 -0700

    swagger url updated

--- swagger.js
+++ swagger.js
@@ -5,7 +5,7 @@ const doc = {
     title: "My API",
     description: "Temple API",
   },
-  host: "cse341-7955.onrender.com",
+  host: "https://cse341-7955.onrender.com",
   schemes: ["https"],
 };
 
