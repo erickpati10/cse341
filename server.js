ab4e64265cd288679f3cb910c07ba03b3ed0e71b
erickpati10 <pat21018@byui.edu>
2024-02-17 04:07:40 -0700

    server file error fixed

--- server.js
+++ server.js
@@ -9,13 +9,15 @@ server
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    next();
     res.setHeader(
       "Access-Control-Allow-Headers",
       "Origin, X-Requested-With, Content-Type, Accept, Z-Key"
     );
-    res.setHeader("COntent-Type", "application/json");
-    res.setHeader("Access-Control-Allow-Methods", "GET, PUT, DELETE, OPTIONS");
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
     next();
   })
   .use("/", require("./routes"));
