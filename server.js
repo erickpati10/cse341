2d340da57954d26a86c647f83e720c9ec42605fb
erickpati10 <pat21018@byui.edu>
2024-02-17 16:01:49 -0700

    server file updated

--- server.js
+++ server.js
@@ -3,31 +3,21 @@ const bodyParser = require("body-parser");
 const mongodb = require("./db/connect");
 
 const port = process.env.PORT || 8080;
-const server = express();
+const app = express();
 
-server
+app
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept, Z-Key",
-    );
-    res.setHeader("Content-Type", "application/json");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET, POST, PUT, DELETE, OPTIONS",
-    );
     next();
   })
   .use("/", require("./routes"));
 
-mongodb.initDb((err, db) => {
+mongodb.initDb((err) => {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
-    server.listen(port, () => {
-      console.log(`Connected to DB and listening on ${port}`);
-    });
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
   }
 });
