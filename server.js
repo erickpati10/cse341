9258269ae08bb99c652556fb6ac87ae61c3919ee
erickpati10 <pat21018@byui.edu>
2024-02-18 00:50:48 -0700

    server file update

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ server
   .use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    next();
   })
   .use('/', require('./routes'));
 
