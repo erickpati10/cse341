910456fc267eecc0a3aef2553e4ac898d25e48b5
erickpati10 <pat21018@byui.edu>
2024-02-17 21:59:15 -0700

    path updated from server file

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongodb = require("./db/connect");
+const routes = require("./routes");
 
 const port = process.env.PORT || 8080;
 const server = express();
@@ -9,6 +10,15 @@ server
   .use(bodyParser.json())
   .use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept, Z-Key",
+    );
+    res.setHeader("Content-Type", "application/json");
+    res.setHeader(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS",
+    );
     next();
   })
   .use("/", require("./routes"));
